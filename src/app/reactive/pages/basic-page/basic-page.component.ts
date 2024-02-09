a99af053c67b221374460e00ee264c741b5def5a
Jose Ramirez <jramireztolentino@gmail.com>
2024-02-09 11:07:14 -0400

    Add formBuilder in basic page component

--- src/app/reactive/pages/basic-page/basic-page.component.ts
+++ src/app/reactive/pages/basic-page/basic-page.component.ts
@@ -1,7 +1,29 @@
 import { Component } from '@angular/core';
 
+import { FormBuilder, FormControl, FormGroup } from '@angular/forms';
+
 @Component({
   selector: 'app-basic-page',
   templateUrl: './basic-page.component.html'
 })
-export class BasicPageComponent { }
+export class BasicPageComponent {
+
+  // myForm: FormGroup = new FormGroup({
+  //   name: new FormControl( '' ),
+  //   price: new FormControl( 0 ),
+  //   inStorage: new FormControl( 0 ),
+  // })
+
+  myForm: FormGroup = this.fb.group({
+    name: [ '' ],
+    price: [ 0 ],
+    inStorage: [ 0 ],
+  })
+
+  constructor( private fb: FormBuilder ) {}
+
+  onSave(): void {
+    console.log(this.myForm.value);
+  }
+
+}
