6ba3f691751e5bc446fa6deeb8b6678dff258cf1
Jose Alberto Ramirez <33133680+jalbertort@users.noreply.github.com>
2024-02-09 10:41:38 -0400

    Add side bar menu (#2)

---
+++ src/app/shared/components/side-menu/side-menu.component.ts
@@ -0,0 +1,24 @@
+import { Component } from '@angular/core';
+
+interface MenuItem {
+  title: string;
+  route: string;
+}
+
+@Component({
+  selector: 'shared-side-menu',
+  templateUrl: './side-menu.component.html'
+})
+export class SideMenuComponent {
+
+  public reactiveMenu: MenuItem[] = [
+    {title: 'Básico', route: './reactive/basic' },
+    {title: 'Dinámico', route: './reactive/dynamic' },
+    {title: 'Switches', route: './reactive/switches' },
+  ]
+
+  public authMenu: MenuItem[] = [
+    {title: 'Registro', route: './auth' },
+  ]
+
+}
