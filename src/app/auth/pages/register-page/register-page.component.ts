19358658b23506620a065aa7ebd181ded2777171
Jose Ramirez <jramireztolentino@gmail.com>
2024-02-12 13:54:52 -0400

    End seccion validators form pages

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -3,6 +3,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 // import * as customValidators from '../../../shared/validators/validators';
 
 import { ValidatorsService } from '../../../shared/services/validators.service';
+import { EmailValidator } from '../../../shared/validators/email-validator.service';
 
 @Component({
   selector: 'app-register-page',
@@ -12,15 +13,20 @@ export class RegisterPageComponent {
 
   myform: FormGroup = this.fb.group({
     name: [ '', [ Validators.required, Validators.pattern( this.validatorsService.firstNameAndLastnamePattern ) ]],
-    email: [ '', [ Validators.required, Validators.pattern( this.validatorsService.emailPattern ) ]],
+    email: [ '', [ Validators.required ], [ this.emailValidator ]],
     user: [ '', [ Validators.required, this.validatorsService.cantBeStrider ]],
     password: [ '', [ Validators.required, Validators.minLength(6) ]],
     password2: [ '', [ Validators.required ]],
+  }, {
+    validators: [
+      this.validatorsService.isFieldOneEqualFieldTwo( 'password', 'password2' )
+    ]
   });
 
   constructor(
     private fb: FormBuilder,
-    private validatorsService: ValidatorsService
+    private validatorsService: ValidatorsService,
+    private emailValidator: EmailValidator
   ) {}
 
   isValidField( field: string): boolean | null {
@@ -33,6 +39,9 @@ export class RegisterPageComponent {
       this.myform.markAllAsTouched();
       return;
     }
+
+    console.log(this.myform.value);
+
 
   }
 
