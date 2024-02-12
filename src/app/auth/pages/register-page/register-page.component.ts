16fd0db00a2eece97e2db91d1a64774e6347bbf1
Jose Ramirez <jramireztolentino@gmail.com>
2024-02-12 12:11:55 -0400

    Add first validators for register user in form page

--- src/app/auth/pages/register-page/register-page.component.ts
+++ src/app/auth/pages/register-page/register-page.component.ts
@@ -1,7 +1,35 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { cantBeStrider } from '../../../shared/validators/validators';
 
 @Component({
   selector: 'app-register-page',
   templateUrl: './register-page.component.html'
 })
-export class RegisterPageComponent { }
+export class RegisterPageComponent {
+
+  myform: FormGroup = this.fb.group({
+    name: [ '', [ Validators.required ]],
+    email: [ '', [ Validators.required, Validators.email ]],
+    user: [ '', [ Validators.required, cantBeStrider ]],
+    password: [ '', [ Validators.required, Validators.minLength(6) ]],
+    password2: [ '', [Validators.required ]],
+  });
+
+  constructor( private fb: FormBuilder) {}
+
+  isVAlidField( field: string): boolean | null {
+    return this.myform.controls[field].errors
+      && this.myform.controls[field].touched
+  }
+
+  onSugmit() {
+
+    if( !this.myform.valid ) {
+      this.myform.markAllAsTouched();
+      return;
+    }
+
+  }
+
+ }
